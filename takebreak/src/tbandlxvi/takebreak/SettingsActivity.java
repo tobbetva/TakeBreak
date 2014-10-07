ce3e2fcbf52adfef47faab93c4c83e0005e67728
tobbetva <tobbetva@users.noreply.github.com>
2014-10-07 14:08:32 +0200

    Changed Settings into Fragment style rather than PreferenceActivity style.

--- takebreak/src/tbandlxvi/takebreak/SettingsActivity.java
+++ takebreak/src/tbandlxvi/takebreak/SettingsActivity.java
@@ -1,10 +1,9 @@
 package tbandlxvi.takebreak;
 
-import tbandlxvi.takebreak.R;
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity
+public class SettingsActivity extends Activity
 {
   public static final String PREFKEY_WORK_PHASE_LENGTH = "prefkey_work_phase_length";
   public static final String PREFKEY_REST_PHASE_LENGTH = "prefkey_rest_phase_length";
@@ -14,15 +13,15 @@ public class SettingsActivity extends PreferenceActivity
   public static final String PREFKEY_AUTO_PHASE_CHANGE = "prefkey_auto_phase_change";
   public static final String PREFKEY_KEEP_PHONE_AWAKE = "prefkey_keep_phone_awake";
 
-  @SuppressWarnings("deprecation")
   @Override
   protected void onCreate(Bundle savedInstanceState) 
   {
     super.onCreate(savedInstanceState);
-
-    // Load the preferences from an XML resource
-    addPreferencesFromResource(R.xml.preferences); // Must be used for Android 2.3.3 support.
+   
+    // Display the fragment as the main content.
+    getFragmentManager().beginTransaction()
+            .replace(android.R.id.content, new SettingsFragment())
+            .commit();
     
-    this.setTitle(getResources().getString(R.string.str_settings_caption));
   }  
 }
